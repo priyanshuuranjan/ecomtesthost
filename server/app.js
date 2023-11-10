85b9539c2545b9ce66c4e11c6c1b87b94343781a
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-11-11 00:43:19 +0530

    Working on Deployment

--- server/app.js
+++ server/app.js
@@ -30,8 +30,8 @@ app.post("/api/create-checkout-session", async (req, res) => {
     payment_method_types: ["card"],
     line_items: lineItems,
     mode: "payment",
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cancel",
+    success_url: "kumarfertilizer.netlify.app/success",
+    cancel_url: "kumarfertilizer.netlify.app/cancel",
   });
 
   res.json({ id: session.id });
